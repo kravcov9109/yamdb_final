0fa8e685cc6d34dd0d5d4246ba7eccbccfbba4ea
kravcov9109 <kravcov9109@yandex.ru>
2021-03-01 22:39:35 +0200

    My 36 commit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG: 
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, '/yamdb_static/static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
